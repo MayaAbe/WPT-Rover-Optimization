bfb9a135cdd2fefb0e35ff3eb7aad0f3c123f988
ABema <a.maya3141592@icloud.com>
2023-08-21 09:33:25 +0900

    Sim: Add thermal calculator

---
+++ simulation/test.py
@@ -0,0 +1,5 @@
+import thermal
+
+print(thermal.thermal_transit(100000))
+a=thermal.thermal_transit(100000)[0]
+print(a)
--- simulation/thermal.py
+++ simulation/thermal.py
@@ -2,46 +2,43 @@ import time
 from math import log, atan, sqrt 
 from matplotlib import pyplot as plt
 
-def thermal_transit(
-        passed_time=10, #経過時間
-        emissivity_rover=0.655, #ローバー放射率
-        C=1000,  #ローバー熱容量
-        T_initial=300,  #ローバー初期温度
-        T_env=100,  #周囲の環境温度
-        Q_in=100,  #内部発熱
-        threshold=0.001, #定常状態とみなす閾値
-        vertical=0.3, #ローバー高さと幅
-        horizontal=0.45, #ローバー奥行
-        distance=0.15, #ローバーの地面との距離
-        emissivity_regolith=0.925, #レゴリスの放射率 
-        S_top=0.135,  #上面の面積
-        S_side=0.450,  #側面積
-        S_bottom=0.135, #低面積
-        
-        d=0.15,  # 物質の厚み (m)
-        k=130,  # 熱伝導率 (W/mK)
-        A=0.000256  # 温度の伝導面積 (m^2)
-):
+start_time = time.time()
+
+def thermal_transit(passed_time=1000,
+        emissivity_rover=0.655,
+        T_initial=300,  
+        T_env=100,  
+        C=1000,  
+        Q_in=100,  
+        threshold=0.001,    
+        vertical=0.3,  
+        horizontal=0.45,  
+        distance=0.15,  
+        emissivity_regolith=0.925,  
+        S_top=0.135,  
+        S_side=0.450,  
+        S_bottom=0.135,
+        d=0.15,
+        k=130,
+        A=0.000256):
+
     sigma = 5.67 * 10**-8  
     
     T = T_initial
     time = 0
-    dt = 0.001  
+    dt = 1  
 
     x = vertical / distance
     y = horizontal / distance
 
-    F_ij = log(((1+x**2)*(1+y**2))/(1+x**2+y**2)) + \
-           x**2*log((x**2 * (1+x**2+y**2))/((1+x**2)*(x**2+y**2))) + \
-           y**2*log((y**2 * (1+x**2+y**2))/((1+y**2)*(x**2+y**2))) + \
-           4*x*atan(1/x) + 4*y*atan(1/y) - 4*sqrt(x**2+y**2)*atan(1/sqrt(x**2+y**2))
+    F_ij = log(((1+x**2)*(1+y**2))/(1+x**2+y**2)) +            x**2*log((x**2 * (1+x**2+y**2))/((1+x**2)*(x**2+y**2))) +            y**2*log((y**2 * (1+x**2+y**2))/((1+y**2)*(x**2+y**2))) +            4*x*atan(1/x) + 4*y*atan(1/y) - 4*sqrt(x**2+y**2)*atan(1/sqrt(x**2+y**2))
 
     R1 = emissivity_rover * S_top
     R2 = emissivity_rover * emissivity_regolith * S_side
     R3 = emissivity_rover * emissivity_regolith * S_bottom * F_ij
 
-    times = [time]
-    temperatures = [T]
+    time_below_273 = 0
+    dT_when_below_273 = 0
 
     while time <= passed_time:
         Q_radiation = (R1+R2+R3) * sigma * (T**4 - T_env**4)
@@ -52,9 +49,14 @@ def thermal_transit(
         dT = (Q_in - Q_out) * dt / C
         T += dT
         time += dt
-        times.append(time)
-        temperatures.append(T)
 
+        if T < 273 and time_below_273 == 0:
+            time_below_273 = passed_time - time
+            dT_when_below_273 = dT/dt
+
+    return T-273, time_below_273, dT_when_below_273
+
+    """
     plt.figure(figsize=(10, 6))
     plt.plot(times, temperatures)
     plt.xlabel('Time (s)')
@@ -66,4 +68,7 @@ def thermal_transit(
     return T-273
 
 # テスト関数の実行
-thermal_transit()
+print(thermal_transit())
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"実行時間: {elapsed_time} 秒")"""
